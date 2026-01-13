e2b04e52bc8d70e302060c767d8c2abab3e2a00a
Soniya Eknath Girhe <girhesoniya@gmail.com>
2026-01-13 23:30:08 +0530

    Add Message and Mssage1 Java classes
    Introduced Message.java with a simple main method printing a message, and Mssage1.java with a show method and main method to demonstrate class usage.

---
+++ OOPS-Java/0th_Practical/Mssage1.java
@@ -0,0 +1,9 @@
+class Mssage1 {
+    void show() {
+        System.out.println("This is Mssage1 class");
+    }
+    public static void main(String[] args) {
+        Mssage1 obj = new Mssage1();
+        obj.show();
+    }
+}
